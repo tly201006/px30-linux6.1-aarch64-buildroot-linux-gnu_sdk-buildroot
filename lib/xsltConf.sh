b527c4d9ce4d9043c8b070700e98e4096388f2a9
tangly <tangly@smatek.com.cn>
2025-12-11 18:45:05 +0800

    init

---
+++ lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/tangly/work2/rklinux/px30_linux6.1_20250725/buildroot/output/rockchip_px30_64/host/lib"
+XSLT_LIBS="-lxslt -L/home/tangly/work2/rklinux/px30_linux6.1_20250725/buildroot/output/rockchip_px30_64/host/lib -lxml2  "
+XSLT_PRIVATE_LIBS="-lm"
+XSLT_INCLUDEDIR="-I/home/tangly/work2/rklinux/px30_linux6.1_20250725/buildroot/output/rockchip_px30_64/host/include"
+MODULE_VERSION="xslt-1.1.39"
---
+++ share/gdb/auto-load/home/tangly/work2/rklinux/px30_linux6.1_20250725/buildroot/output/rockchip_px30_64/host/lib/libgobject-2.0.so.0.7600.1-gdb.py
@@ -0,0 +1,10 @@
+import sys
+import gdb
+
+# Update module path.
+dir_ = '/home/tangly/work2/rklinux/px30_linux6.1_20250725/buildroot/output/rockchip_px30_64/host/share/glib-2.0/gdb'
+if not dir_ in sys.path:
+    sys.path.insert(0, dir_)
+
+from gobject_gdb import register
+register (gdb.current_objfile ())
